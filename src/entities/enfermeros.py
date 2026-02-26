8d36eed424e90c06a01433dc96c44a5c0b0c8581
Paulifb <paulinafranco328674@correo.itm.edu.co>
2026-02-25 23:27:47 -0500

    feat: modificar la clase enfermero agregando herencia de la clase persona

--- src/entities/enfermeros.py
+++ src/entities/enfermeros.py
@@ -1,19 +1,22 @@
-class Enfermero:
+from .persona import Persona
+
+
+class Enfermero(Persona):
 
     def __init__(
-        self, id_enfermero: int, nombre: str, especialidad: str, id_sede: int
+        self, id_persona: int, nombre: str, especialidad: str, id_sede: int
     ) -> None:
         """
         Inicializa un objeto Enfermero.
 
         Args:
-            id_enfermero (int): Identificador único del enfermero.
-            nombre (str): Nombre del enfermero.
+            id_persona (int): Identificador único heredado de Persona.
+            nombre (str): Nombre completo heredado de Persona.
             especialidad (str): Área o especialidad en la que trabaja.
             id_sede (int): Identificador de la sede donde labora
         """
-        self._id_enfermero = id_enfermero
-        self._nombre = nombre
+        super().__init__(id_persona, nombre)
+
         self._especialidad = especialidad
         self._id_sede = id_sede
 
